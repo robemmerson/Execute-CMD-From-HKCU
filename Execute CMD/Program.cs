18a178190057702d15608c2dd1135e1af592c852
Rob Emmerson <rob@scriptingsolutions.co>
2017-03-23 02:00:06 +0000

    Add project files.

---
+++ Execute CMD/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execute_CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
